47976d7eccfbd85491397a634f668c2780b3636d
MiauDona <donanferyt@gmail.com>
2026-02-15 17:44:50 +0100

    chore: poner informacion en el perfilViewModel

--- composeApp/src/commonMain/kotlin/dev/wdona/burnt_out/viewmodels/PerfilViewModel.kt
+++ composeApp/src/commonMain/kotlin/dev/wdona/burnt_out/viewmodels/PerfilViewModel.kt
@@ -1,7 +1,25 @@
 package dev.wdona.burnt_out.viewmodels
 
+import dev.wdona.burnt_out.shared.BurntOutSDK
 import dev.wdona.burnt_out.shared.cache.DatabaseDriverFactory
+import dev.wdona.burnt_out.shared.network.Tablero
+import dev.wdona.burnt_out.shared.network.Usuario
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
-class PerfilViewModel(driverFactory: DatabaseDriverFactory) {
-    
+class PerfilViewModel(databaseDriverFactory: DatabaseDriverFactory) {
+    private val sdk = BurntOutSDK(databaseDriverFactory)
+    // Crea un CoroutineScope
+    private val viewModelScope = CoroutineScope(Dispatchers.Main)
+
+    // Crea un MutableStateFlow (para actualizar el estado en la UI) privado y mutable para la respuesta
+    private val _uiState = MutableStateFlow<Usuario?>(null)
+
+    // Crea un StateFlow publico y de solo lectura para la respuesta
+    val uiState: StateFlow<Usuario?> = _uiState.asStateFlow()
+    private val _listaUsuarios = MutableStateFlow<List<Usuario>>(emptyList())
+    val listaUsuarios: StateFlow<List<Usuario>> = _listaUsuarios
 }
