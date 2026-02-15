b84941a16454f92cd0f00c5eddd37e73c1820616
MiauDona <donanferyt@gmail.com>
2026-02-15 22:27:13 +0100

    feat: Anadir boton de crear tarea en cada tablero

--- composeApp/src/commonMain/kotlin/dev/wdona/burnt_out/pantallas/tablero/DetalleTableroScreen.kt
+++ composeApp/src/commonMain/kotlin/dev/wdona/burnt_out/pantallas/tablero/DetalleTableroScreen.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -23,6 +25,7 @@ import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import dev.wdona.burnt_out.components.common.BotonVolver
 import dev.wdona.burnt_out.components.tarea.CardTarea
+import dev.wdona.burnt_out.pantallas.tarea.MenuCrearTareaScreen
 import dev.wdona.burnt_out.viewmodelfactories.TareaViewModelFactory
 import dev.wdona.burnt_out.viewmodels.TareaViewModel
 
@@ -42,13 +45,25 @@ class DetalleTableroScreen(val idTablero: Long, val nombreTablero: String, val t
             tareaViewModel.cargarTareasPorTablero(idTablero)
         }
 
-        DetalleTableroContent(tareaViewModel, nombreTablero) { navigator.pop() }
+        DetalleTableroContent(tareaViewModel = tareaViewModel,
+            nombreTablero = nombreTablero,
+            idTablero = idTablero,
+            onVolver = { navigator.pop() },
+            onIrACrearTarea = {
+                navigator.push(
+                    MenuCrearTareaScreen(
+                        factory = tareaViewModelFactory,
+                        idTablero = idTablero
+                    )
+                )
+            }
+        )
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
 @Composable
-fun DetalleTableroContent(tareaViewModel: TareaViewModel, nombreTablero: String, onVolver: () -> Unit) {
+fun DetalleTableroContent(tareaViewModel: TareaViewModel, nombreTablero: String, idTablero: Long ,onVolver: () -> Unit, onIrACrearTarea: (Long) -> Unit) {
     val listaTareas by tareaViewModel.listaTareas.collectAsState()
 
     Scaffold(
@@ -64,6 +79,11 @@ fun DetalleTableroContent(tareaViewModel: TareaViewModel, nombreTablero: String,
         Column(
             modifier = Modifier.padding(paddingValues)
         ) {
+            Button(
+                onClick = { onIrACrearTarea(idTablero) },
+            ) {
+                Text("Crear tarea")
+            }
             LazyColumn(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.spacedBy(12.dp),
@@ -73,7 +93,7 @@ fun DetalleTableroContent(tareaViewModel: TareaViewModel, nombreTablero: String,
 
             ) {
                 items(listaTareas) { tarea ->
-                    CardTarea(tarea.titulo, tarea.descripcion)
+                    CardTarea(tarea.titulo)
                 }
             }
         }
