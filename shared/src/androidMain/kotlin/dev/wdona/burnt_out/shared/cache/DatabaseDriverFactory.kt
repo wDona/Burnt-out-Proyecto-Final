b55bb0db0c763036824958cbfae1a7890ff2e9a2
miaudona <donanferyt@gmail.com>
2026-02-16 08:45:32 +0100

    fix: implementacion del DatabaseDriverFactory en Android

--- shared/src/androidMain/kotlin/dev/wdona/burnt_out/shared/cache/DatabaseDriverFactory.kt
+++ shared/src/androidMain/kotlin/dev/wdona/burnt_out/shared/cache/DatabaseDriverFactory.kt
@@ -3,9 +3,26 @@ package dev.wdona.burnt_out.shared.cache
 import android.content.Context
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
+import java.io.File
 
 actual class DatabaseDriverFactory(private val context: Context) {
     actual fun createDriver(): SqlDriver {
-        return AndroidSqliteDriver(AppDatabase.Schema, context, "test.db")
+        val databasePath = "burnt_out.db"
+        val dbFile = File(databasePath)
+        val driver: SqlDriver = AndroidSqliteDriver(AppDatabase.Schema, context, databasePath)
+
+        val database = AppDatabase(driver)
+        if (database.appDatabaseQueries.getOrganizacionById(1).executeAsOneOrNull() == null) {
+            insertarDatosIniciales(database)
+        }
+
+        return driver
+    }
+
+    private fun insertarDatosIniciales(database: AppDatabase) {
+        database.appDatabaseQueries.insertOrgbase()
+        database.appDatabaseQueries.insertAjustes()
+        database.appDatabaseQueries.insertEquipoBase()
+        database.appDatabaseQueries.insertUsuarioBase()
     }
 }
