d8d4c834840e4e9faa5534e06f081bce887ffc39
miaudona <donanferyt@gmail.com>
2026-02-16 10:53:36 +0100

    style: anadidos los iconos para android y pc de inicio

--- composeApp/src/jvmMain/kotlin/dev/wdona/burnt_out/main.kt
+++ composeApp/src/jvmMain/kotlin/dev/wdona/burnt_out/main.kt
@@ -1,7 +1,10 @@
 package dev.wdona.burnt_out
 
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import burnt_out.composeapp.generated.resources.Res
+import burnt_out.composeapp.generated.resources.logoBurntOutIcon
 import dev.wdona.burnt_out.viewmodelfactories.EquipoViewModelFactory
 import dev.wdona.burnt_out.viewmodelfactories.PerfilViewModelFactory
 import dev.wdona.burnt_out.viewmodelfactories.TableroViewModelFactory
@@ -11,7 +14,9 @@ fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         alwaysOnTop = true,
-        title = "Burnt-out",
+        title = "Burnt out",
+        icon = org.jetbrains.compose.resources.painterResource(Res.drawable.logoBurntOutIcon)
+
     ) {
         val tareaViewModelFactory = TareaViewModelFactory()
         val equipoViewModelFactory = EquipoViewModelFactory()
